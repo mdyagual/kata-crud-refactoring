7411ad1a31b52fd075920afe30829f3a3e9c81cd
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-24 03:56:03 -0500

    Implementación del DTO y con sus respectivas transformaciones y refactorización de los métodos

--- back/src/main/java/co/com/sofka/crud/services/TodoService.java
+++ back/src/main/java/co/com/sofka/crud/services/TodoService.java
@@ -1,8 +1,12 @@
 package co.com.sofka.crud.services;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.com.sofka.crud.dto.TodoDTO;
 import co.com.sofka.crud.entities.Todo;
 import co.com.sofka.crud.repositories.TodoRepository;
 
@@ -10,22 +14,49 @@ import co.com.sofka.crud.repositories.TodoRepository;
 public class TodoService {
 
     @Autowired
-    private TodoRepository repository;
+    private TodoRepository toDoRepo;
 
-    public Iterable<Todo> list(){
-        return repository.findAll();
+    //Gets
+    public ArrayList<TodoDTO> getToDos(){
+        return (ArrayList<TodoDTO>) toDoRepo.findAll()
+                .stream()                    
+                .map(this::convertEntityToDto)
+                .collect(Collectors.toList());
+    }
+    public Todo getToDo(Long id){
+        return toDoRepo.findById(id).orElseThrow();
+    }
+    
+    //Save
+    public Todo saveToDO(TodoDTO todoDTO){
+        return toDoRepo.save(convertDTOToEntity(todoDTO));
     }
 
-    public Todo save(Todo todo){
-        return repository.save(todo);
+    //Delete
+    public void deleteToDo(Long id){
+        toDoRepo.delete(getToDo(id));
+    }    
+
+    //Conversiones
+    private TodoDTO convertEntityToDto(Todo todo){
+        TodoDTO todoDTO = new TodoDTO();
+
+        todoDTO.setName(todo.getName());
+        todoDTO.setCompleted(todo.isCompleted());
+        todoDTO.setGroupListId(todo.getGroupListId());
+
+        return todoDTO;
     }
 
-    public void delete(Long id){
-        repository.delete(get(id));
+    private Todo convertDTOToEntity(TodoDTO todoDTO){
+        Todo todo= new Todo();
+
+        todo.setName(todoDTO.getName());
+        todo.setCompleted(todoDTO.isCompleted());
+        todo.setGroupListId(todoDTO.getGroupListId());
+
+        return todo;
     }
 
-    public Todo get(Long id){
-         return repository.findById(id).orElseThrow();
-    }
 
 }
