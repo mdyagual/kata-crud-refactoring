2fec2b6c3dad9f9ce582d53d286c8a9917b42780
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-24 04:11:41 -0500

    Refactorización del controlador para uso de DTO y designación de endpoints apropiados

--- back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
+++ back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
@@ -3,42 +3,55 @@ package co.com.sofka.crud.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import co.com.sofka.crud.dto.TodoDTO;
 import co.com.sofka.crud.entities.Todo;
 import co.com.sofka.crud.services.TodoService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/todos")
 public class TodoController {
 
     @Autowired
-    private TodoService service;
+    private TodoService toDoService;
 
-    @GetMapping(value = "api/todos")
-    public Iterable<Todo> list(){
-        return service.list();
+    //GET
+    @GetMapping("/all")
+    //http://127.0.0.1:8080/api/todos/all  
+    public Iterable<TodoDTO> obtenerTodos(){
+        return toDoService.getToDos();
+    }
+
+    @GetMapping("/todo/{id}")
+    //http://127.0.0.1:8080/api/todos/todo/{id}
+    public Todo obtenerTodoPorId(@RequestParam("id") Long id){
+        return toDoService.getToDo(id);
     }
     
-    @PostMapping(value = "api/todo")
-    public Todo save(@RequestBody Todo todo){
-        return service.save(todo);
+    //POST
+    @PostMapping("/guardar")
+    //http://127.0.0.1:8080/api/todos/guardar
+    public Todo guardarTodo(@RequestBody TodoDTO todoDTO){
+        return toDoService.saveToDO(todoDTO);
     }
 
-    @PutMapping(value = "api/todo")
-    public Todo update(@RequestBody Todo todo){
-        if(todo.getId() != null){
-            return service.save(todo);
+    //PUT
+    @PutMapping("/actualizar")
+    //http://127.0.0.1:8080/api/todos/actualizar
+    public Todo update(@RequestBody TodoDTO todoDTO){
+        if(todoDTO.getId() != null){
+            return toDoService.saveToDO(todoDTO);
         }
         throw new RuntimeException("No existe el id para actualziar");
     }
 
-    @DeleteMapping(value = "api/{id}/todo")
-    public void delete(@PathVariable("id")Long id){
-        service.delete(id);
+    //DELETE
+    @DeleteMapping(path= "/eliminar/{id}")
+    //http://127.0.0.1:8080/api/todos/eliminar/{id}
+    public void eliminarToDo(@PathVariable("id") Long id){
+        toDoService.deleteToDo(id);
     }
 
-    @GetMapping(value = "api/{id}/todo")
-    public Todo get(@PathVariable("id") Long id){
-        return service.get(id);
-    }
+    
 
 }
