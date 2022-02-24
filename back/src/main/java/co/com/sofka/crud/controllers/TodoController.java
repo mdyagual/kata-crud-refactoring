f7b3b1dade1879e9622016aef7d51a8b4bc55a4d
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-24 04:35:31 -0500

    Refactorización utilizando ResponseEntities en cada operación CRUD para tener respuestas de las poperaciones más concisas

--- back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
+++ back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
@@ -1,6 +1,10 @@
 package co.com.sofka.crud.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import co.com.sofka.crud.dto.TodoDTO;
@@ -18,38 +22,44 @@ public class TodoController {
     //GET
     @GetMapping("/all")
     //http://127.0.0.1:8080/api/todos/all  
-    public Iterable<TodoDTO> obtenerTodos(){
-        return toDoService.getToDos();
+    public ResponseEntity<ArrayList<TodoDTO>> obtenerTodos(){
+        ArrayList<TodoDTO> allToDos = toDoService.getToDos();
+        return new ResponseEntity<ArrayList<TodoDTO>>(allToDos, HttpStatus.OK);
     }
 
     @GetMapping("/todo/{id}")
     //http://127.0.0.1:8080/api/todos/todo/{id}
-    public Todo obtenerTodoPorId(@RequestParam("id") Long id){
-        return toDoService.getToDo(id);
+    public ResponseEntity<TodoDTO> obtenerTodoPorId(@RequestParam("id") Long id){
+        TodoDTO todoDTO = toDoService.getToDoById(id);
+        return new ResponseEntity<TodoDTO>(todoDTO,HttpStatus.OK);
     }
     
     //POST
     @PostMapping("/guardar")
     //http://127.0.0.1:8080/api/todos/guardar
-    public Todo guardarTodo(@RequestBody TodoDTO todoDTO){
-        return toDoService.saveToDO(todoDTO);
+    public ResponseEntity<Todo> guardarTodo(@RequestBody TodoDTO todoDTO){
+        Todo todo = toDoService.saveToDO(todoDTO);
+        return new ResponseEntity<Todo>(todo, HttpStatus.CREATED);
     }
 
     //PUT
     @PutMapping("/actualizar")
     //http://127.0.0.1:8080/api/todos/actualizar
-    public Todo update(@RequestBody TodoDTO todoDTO){
+    public ResponseEntity<Todo> actualizarToDo(@RequestBody TodoDTO todoDTO){
         if(todoDTO.getId() != null){
-            return toDoService.saveToDO(todoDTO);
+            Todo todoUp = toDoService.saveToDO(todoDTO);
+            return new ResponseEntity<Todo>(todoUp,HttpStatus.CREATED);
         }
+        
         throw new RuntimeException("No existe el id para actualziar");
     }
 
     //DELETE
     @DeleteMapping(path= "/eliminar/{id}")
     //http://127.0.0.1:8080/api/todos/eliminar/{id}
-    public void eliminarToDo(@PathVariable("id") Long id){
+    public ResponseEntity eliminarToDo(@PathVariable("id") Long id){
         toDoService.deleteToDo(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     
