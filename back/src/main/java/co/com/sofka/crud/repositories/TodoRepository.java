5583c1ede2e35ac3728c6047defbc92373b70128
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-24 03:55:09 -0500

    Implementación de método abstracto para devolución de todos los ToDos siendo tiÂpo List

--- back/src/main/java/co/com/sofka/crud/repositories/TodoRepository.java
+++ back/src/main/java/co/com/sofka/crud/repositories/TodoRepository.java
@@ -1,8 +1,13 @@
 package co.com.sofka.crud.repositories;
 
+
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
+
 
 import co.com.sofka.crud.entities.Todo;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
+    public abstract ArrayList<Todo> findAll();
 }
