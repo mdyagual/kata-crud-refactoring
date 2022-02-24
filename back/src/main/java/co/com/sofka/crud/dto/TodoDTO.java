f97a15da36388fca3176cfa930d693c801e6ca07
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-24 03:53:37 -0500

    Implementación de DTO para la clase Todo, a espera de ser usada en el servicio y controlador correspondiente

--- back/src/main/java/co/com/sofka/crud/dto/TodoDTO.java
+++ back/src/main/java/co/com/sofka/crud/dto/TodoDTO.java
@@ -1,5 +1,35 @@
 package co.com.sofka.crud.dto;
 
 public class TodoDTO {
+    private Long id;    
+    private String name;
+    private boolean isCompleted;
+    private String groupListId;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+    public String getGroupListId() {
+        return groupListId;
+    }
+    public void setGroupListId(String groupListId) {
+        this.groupListId = groupListId;
+    }
+
     
 }
