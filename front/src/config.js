94b44ec6a08d2e7bab31c7bf95b7765d880a7694
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-24 20:37:10 -0500

    SDefinición de endpoints como constantes para simple llamado

---
+++ front/src/config.js
@@ -0,0 +1,9 @@
+//Para el componente form()
+export const HOST_API = "http://localhost:8080/api/todos";
+
+//Operaciones CRUD - endpoints - Todo
+export const GET_API = HOST_API + "/all";
+export const POST_API = HOST_API + "/guardar";
+export const PUT_API = HOST_API + "/actualizar";
+export const DELETE_API = HOST_API + "/eliminar/";
+
