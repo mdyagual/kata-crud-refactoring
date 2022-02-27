19deccd10a7ab49b0a445dfe38f1d4eaaf5e8ff0
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-27 06:05:06 -0500

    Agregación de endpoint para categoria y separación entre categoria y todo

--- front/src/config.js
+++ front/src/config.js
@@ -1,9 +1,16 @@
 //Para el componente form()
-export const HOST_API = "http://localhost:8080/api/todos";
+export const HOST_API = "http://localhost:8080";
+export const TODO_API ="/api/todos";
+export const CAT_API="/api/categories";
 
 //Operaciones CRUD - endpoints - Todo
-export const GET_API = HOST_API + "/all";
-export const POST_API = HOST_API + "/guardar";
-export const PUT_API = HOST_API + "/actualizar";
-export const DELETE_API = HOST_API + "/eliminar/";
+export const GET_API_TODO = HOST_API + TODO_API+ "/all";
+export const POST_API_TODO = HOST_API + TODO_API+ "/guardar";
+export const PUT_API_TODO = HOST_API +  TODO_API+"/actualizar";
+export const DELETE_API_TODO = HOST_API +  TODO_API+"/eliminar/";
+
+//Operaciones CRUD - endpoints -Categories
+export const GET_API_CAT = HOST_API + CAT_API+ "/all";
+export const POST_API_CAT = HOST_API + CAT_API+ "/guardar";
+export const DELETE_API_CAT = HOST_API +  CAT_API+"/eliminar/";
 
