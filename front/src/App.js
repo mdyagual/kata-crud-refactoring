dceca1981a1d9c6d911cd01a2d920df633b3b787
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-27 23:17:44 -0500

    Actualización de componentes a integrar

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,10 @@
-import React,{useState } from 'react';
-import { CategoryForm, ListCatForm, ToDoForm } from './components/CategoryForm'; //Form2.jsx
-
+import React from 'react';
 import { StoreProvider } from './components/StoreProvider';
 import ToDoPage from './pages/ToDoPage';
 //Corregir que se muestre si tiene para enseñar {!hideComponent ?: "Nada que mostrar"} {!hideComponent ? <List/>: null}        
 //{!hideComponent ? <ToDoForm />  : null }
 export default function App() {
-  const [hideComponent,setHideComponent] = useState(true);  
+  //const [hideComponent,setHideComponent] = useState(true);  
   return (
     <StoreProvider>
       <h3 ><div id="dashboardTitle">Dashboard</div></h3>
