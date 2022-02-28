360ef96c2348b60324b651ddac6799191ba1bed1
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-27 23:17:24 -0500

    Implementación del los escenarios correspondientes a operaciones del crud desde el frontend

--- front/src/reducers/toDoReducer.js
+++ front/src/reducers/toDoReducer.js
@@ -1,95 +1,55 @@
 import { TYPES } from "../utils/operations";
 export const toDoInitialState = {
-    toDoS: []/*{ list: [], item: {} }*/,
-    categories: [] /*{list: [], item:{} }*/
+  toDoS: [] /*{ list: [], item: {} }*/,
+  categories: [] /*{list: [], item:{} }*/,
+  updateItem: null 
 };
 
 export function toDoReducer(state, action) {
-    switch (action.type) {
-      //Todo lo que está action es lo que yo mando
-      //CategoryForm
-      //POST_CAT
-      case TYPES.ADD_CAT: 
-        //... (se crea una copia del state): Se necesita cosas inmutables      
-        return{...state, categories: [...state.categories,action.item]}
-      //DELETE_CAT
-      case TYPES.DELETE_CAT:
-        const categoryDel = state.categories;
-        //Hace un nuevo arreglo con el que ya no está
-        //Todos los métodos del ES6 son inmutables
-        const catList = categoryDel.list.filter((item) => {
-                return item.id !== action.id;
-            });
-        return { ...state, categories: catList }
+  switch (action.type) {
+    case TYPES.ADD_CAT:
+      return { ...state, categories: [...state.categories, action.item] };
 
-      case TYPES.GET_CATS:
-        //const catAllList = state.categories;      
-        //console.log("CatAllList: ",action.list);
-        return { ...state, categories: action.list }
+    case TYPES.ADD_TO_DO:
+      return { ...state, toDoS: [...state.toDoS, action.item] };
 
-      case TYPES.GET_TODOS:
-        //TODO: Determinar la categoría, mapearla.
-        /*const todoUpList = state.todo;  
-        todoUpList.list = action.list;   
-        const newList = Object.keys(todoUpList).map((idCat)=>{
-          if(idCat === action.idCat){
-            return action.list;
-          }
-          return todoUpList[idCat];
-        });
-        //console.log("NewList: ",newList);
-        todoUpList.list=newList;        */
-        return { ...state, toDoS: action.list}
-        
-      case TYPES.ADD_TO_DO:
-        /*const todoUp = state.todo.list;
-        todoUp.push(action.item);
-        return { ...state, todo: {list: todoUp.filter((item) => {
-          return (action.id!==item.id  );
-        }), item: {}} }*/
-        return{...state, toDoS: [...state.toDoS,action.item]}
+    case TYPES.GET_CATS:
+      return { ...state, categories: action.list };
 
-      /*case 'update-item':
-        const todoUpItem = state.todo;
-        const listUpdateEdit = todoUpItem.list.map((item) => {
-          if (item.id === action.item.id) {
-            return action.item;
-          }
-          return item;
-        });
-        todoUpItem.list = listUpdateEdit;
-        todoUpItem.item = {};
-        return { ...state, todo: todoUpItem }
+    case TYPES.GET_TODOS:
+      return { ...state, toDoS: action.list };
 
-      case 'delete-item':
-        const todoUpDelete = state.todo;
-        const listUpdate = todoUpDelete.list.filter((item) => {
-          return item.id !== action.id;
-        });
-        todoUpDelete.list = listUpdate;
-        return { ...state, todo: todoUpDelete }
+    case TYPES.DELETE_CAT:
+      const categoryDel = state.categories;
+      const catList = categoryDel.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, categories: catList };
+    
+    case TYPES.DELETE_TO_DO:
+      const toDoDel = state.toDoS;
+      const toDoList = toDoDel.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, toDoS: toDoList };
 
-      case 'update-list':
-        const todoUpList = state.todo;
-        todoUpList.list = action.list;
-        return { ...state, todo: todoUpList }
+    case TYPES.UPDATE_TO_DO:
+      const toDoUp = state.toDoS;
+      const toDoUpList = toDoUp.map((item) => {
+        if(item.id === action.item.id){
+          return action.item;
+        }
+        return item;
+      });
+      return { ...state, toDoS: toDoUpList };
 
-      case 'edit-item':
-        const todoUpEdit = state.todo;
-        todoUpEdit.item = action.item;
-        return { ...state, todo: todoUpEdit }
+     case TYPES.UPDATE_ITEM :
+       return {...state, updateItem: action.item}
 
-      case 'add-item':
-        const todoUp = state.todo.list;
-        todoUp.push(action.item);
-        return { ...state, todo: {list: todoUp, item: {}} }
-      
-      case 'add-list':
-        const todoUpAdd = state.todo;
-        todoUpAdd.catName = action.catName;        
-        return { ...state, todo: todoUpAdd }*/
+    case TYPES.CLEAN_ITEM:
+        return {...state, updateItem: null}
 
-      default:
-        return state;
-    }
+    default:
+      return state;
   }
+}
