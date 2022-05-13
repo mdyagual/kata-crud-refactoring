1b18add86fd2d183756b7be389ff6d1cda40a475
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-05-12 21:59:30 -0500

    AFTER_SENT---Agregación de etiqueta faltante para que se entienda que es una interfaz repositorio, además de la definición del método findAll()

--- back/src/main/java/co/com/sofka/crud/repositories/CategoryRepository.java
+++ back/src/main/java/co/com/sofka/crud/repositories/CategoryRepository.java
@@ -3,7 +3,11 @@ package co.com.sofka.crud.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import co.com.sofka.crud.entities.Category;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Long>{
-    
+    public abstract ArrayList<Category> findAll();
 }
