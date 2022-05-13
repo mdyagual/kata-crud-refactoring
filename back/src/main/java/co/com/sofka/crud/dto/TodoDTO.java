9c0ba3e515d3b98f864de4e3222bda23f52995a8
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-05-12 22:07:55 -0500

    AFTER_SENT---Corrección del DTO e implementación de lombok

--- back/src/main/java/co/com/sofka/crud/dto/TodoDTO.java
+++ back/src/main/java/co/com/sofka/crud/dto/TodoDTO.java
@@ -1,39 +1,14 @@
 package co.com.sofka.crud.dto;
 
-import co.com.sofka.crud.entities.Category;
 
+import lombok.Data;
+
+@Data
 public class TodoDTO {
     private Long id;    
     private String name;
     private boolean isCompleted;
-    private Category category;
-    
-    public Category getCategory() {
-        return category;
-    }
-    public void setCategory(Category c) {
-        this.category = c;
-    }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public boolean isCompleted() {
-        return isCompleted;
-    }
-    public void setCompleted(boolean isCompleted) {
-        this.isCompleted = isCompleted;
-    }
-
-  
+    private Long categoryId;
    
 
     
